b960a3c5afd81f7916e43f8f5bf0b318ea38c95b
Dominik <68446074+Kronicaler@users.noreply.github.com>
2025-08-11 15:13:26 +0200

    first commit

---
+++ board.gd
@@ -0,0 +1,73 @@
+extends Sprite2D
+
+enum State {circle, cross}
+
+var currently_playing: State = State.circle
+signal playing_signal(player: State)
+
+@onready
+var right = $right
+@onready
+var center = $center
+@onready
+var left = $left
+@onready
+var btm = $btm
+@onready
+var btm_right = $btm_right
+@onready
+var btm_left = $btm_left
+@onready
+var top = $top
+@onready
+var top_right = $top_right
+@onready
+var top_left = $top_left
+
+func _ready():
+	playing_signal.emit(currently_playing)
+
+func _on_button_pressed() -> void:
+	match currently_playing:
+		State.circle:
+			currently_playing = State.cross
+			playing_signal.emit(currently_playing)
+		State.cross:
+			currently_playing = State.circle
+			playing_signal.emit(currently_playing)
+	
+	var board = [[top_left,top,top_right], [left,center,right], [btm_left,btm,btm_right]]
+	
+	for i in 3:
+		if board[i][0].state == 1 && board[i][1].state == 1 && board [i][2].state == 1:
+			game_over(State.circle)
+		if board[0][i].state==1 && board[1][i].state==1 && board [2][i].state ==1:
+			game_over(State.circle)
+		if board[i][0].state == 2 && board[i][1].state == 2 && board [i][2].state == 2:
+			game_over(State.cross)
+		if board[0][i].state==2 && board[1][i].state==2 && board [2][i].state ==2:
+			game_over(State.cross)
+	
+	if board[0][0].state==1 && board[1][1].state==1 && board[2][2].state==1:
+		game_over(State.circle)
+	if board[0][2].state==1 && board[1][1].state==1 && board[2][0].state==1:
+		game_over(State.circle)
+	if board[0][0].state==2 && board[1][1].state==2 && board[2][2].state==2:
+		game_over(State.cross)
+	if board[0][2].state==2 && board[1][1].state==2 && board[2][0].state==2:
+		game_over(State.cross)
+
+func game_over(who_won:State):
+	var board = [[top_left,top,top_right], [left,center,right], [btm_left,btm,btm_right]]
+	
+	for i in 3:
+		for j in 3:
+			remove_child(board[i][j])
+	
+	match who_won:
+		State.circle:
+			self.texture=load("res://circle  won.png")
+		State.cross:
+			self.texture=load("res://cross  won.png")
+	
+	$"RestartButton".show()
---
+++ button.gd
@@ -0,0 +1,29 @@
+extends Button
+
+enum State {none, circle, cross}
+enum Playing {circle, cross}
+var circle = load("res://circle.png")
+var cross = load("res://cross.png")
+var state: State = State.none
+var playing: Playing
+
+var sprite: Sprite2D
+
+func _ready():
+	sprite = $"Sprite2D"
+	get_tree().get_first_node_in_group("board").playing_signal.connect(_on_board_playing_signal)
+
+func _pressed():
+	if state != State.none:
+		return
+		
+	match playing:
+		Playing.circle:
+			sprite.texture = circle
+			state = State.circle
+		Playing.cross:
+			sprite.texture = cross
+			state = State.cross
+
+func _on_board_playing_signal(player: int) -> void:
+	playing = player
